7a284e638c49df7e004b345ad0dc6ae986f78e7c
Hesam Alavi <hesamalavi27@gmail.com>
2020-03-16 16:47:17 +1100

    Added methods to counter app

--- src/playground/counter.js
+++ src/playground/counter.js
@@ -1,33 +1,63 @@
-let count = 0;
-const addOne = () => {
-  count++;
-  renderCounterApp();
-};
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddOne = this.handleAddOne.bind(this);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+  handleAddOne() {
+    console.log('handleAddOne');
+  }
+  handleMinusOne() {
+    console.log('handleMinusOne');
+  }
+  handleReset() {
+    console.log('handleReset');
+  }
+  render() {
+    return (
+      <div>
+        <h1>Count: </h1>
+        <button onClick={this.handleAddOne}>+1</button>
+        <button onClick={this.handleMinusOne}>-1</button>
+        <button onClick={this.handleReset}>reset</button>
+      </div>
+    );
+  }
+}
 
-const minusOne = () => {
-  if (count <= 0 ? (count = 0) : count--) renderCounterApp();
-};
+ReactDOM.render(<Counter />, document.getElementById('app'));
 
-const reset = () => {
-  count = 0;
-  renderCounterApp();
-};
+// let count = 0;
+// const addOne = () => {
+//   count++;
+//   renderCounterApp();
+// };
 
-// console.log(templateTwo);
+// const minusOne = () => {
+//   if (count <= 0 ? (count = 0) : count--) renderCounterApp();
+// };
 
-const appRoot = document.getElementById('app');
+// const reset = () => {
+//   count = 0;
+//   renderCounterApp();
+// };
 
-const renderCounterApp = () => {
-  const templateTwo = (
-    <div>
-      <h1>Count: {count}</h1>
+// // console.log(templateTwo);
 
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>reset</button>
-    </div>
-  );
-  ReactDOM.render(templateTwo, appRoot);
-};
+// const appRoot = document.getElementById('app');
 
-renderCounterApp();
+// const renderCounterApp = () => {
+//   const templateTwo = (
+//     <div>
+//       <h1>Count: {count}</h1>
+
+//       <button onClick={addOne}>+1</button>
+//       <button onClick={minusOne}>-1</button>
+//       <button onClick={reset}>reset</button>
+//     </div>
+//   );
+//   ReactDOM.render(templateTwo, appRoot);
+// };
+
+// renderCounterApp();
