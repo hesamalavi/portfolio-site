2c05f915fe392548a1aaf20559514d0f8d5c3ced
Hesam Alavi <hesamalavi27@gmail.com>
2020-03-24 13:38:54 +1100

    Importing react and reactDOM

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-// import './utils.js';
-import { square, add } from './utils';
-import isSenior, { isAdult, canDrink } from './person';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import validator from 'validator';
 
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(4, 4));
+console.log(validator.isEmail('test@test.com'));
 
-console.log(isAdult(7));
-console.log(canDrink(7));
-console.log(isSenior(99));
+const template = React.createElement('p', {}, 'test');
+ReactDOM.render(template, document.getElementById('app'));
+
+// // import './utils.js';
+// import { square, add } from './utils';
+// import isSenior, { isAdult, canDrink } from './person';
+
+// // console.log('app.js is running');
+// // console.log(square(4));
+// // console.log(add(4, 4));
+
+// console.log(isAdult(7));
+// console.log(canDrink(7));
+// console.log(isSenior(99));
