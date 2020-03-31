47e66456a4c813985d4ed52641ebe6a04897d748
Hesam Alavi <hesamalavi27@gmail.com>
2020-04-01 10:59:48 +1100

    Setting up switch and link in react router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './Styles/styles.scss';
 
-ReactDOM.render( < p > This is my boilerplate < /p> , document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>This is from my dashboard component</div>
+);
+const AddExpensePage = () => <div>Add expense</div>;
+
+const EditExpensePage = () => <div>Edit your expense here</div>;
+
+const HelpPage = () => <div>This is the help page</div>;
+
+const NotFoundPage = () => (
+  <div>
+    404 - <Link to="/">Go Home</Link>
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
