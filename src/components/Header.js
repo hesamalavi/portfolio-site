c37eb882d8eadf317bf8e7a916418489a43e77fa
Hesam Alavi <hesamalavi27@gmail.com>
2020-04-01 18:19:04 +1100

    Client side routing done

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,12 @@ import { Link, NavLink } from 'react-router-dom';
 const Header = () => (
   <header>
     <h1>Portfolio</h1>
-    <NavLink to="/" exact={true}>
+    <NavLink to="/" activeClassName="is-active" exact={true}>
       Home
     </NavLink>
-    <NavLink to="/portfolio">Portfolio</NavLink>
+    <NavLink to="/portfolio" activeClassName="is-active" exact={true}>
+      Portfolio
+    </NavLink>
     <NavLink to="/contact">Contact</NavLink>
   </header>
 );
